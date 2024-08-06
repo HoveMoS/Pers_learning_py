0ec91b834e24b7ffa79e27512328c9c5ed382a5e
lostar-surface <redmoonhyh@163.com>
2024-08-06 23:17:21 +0800

    add java.java

---
+++ java.java
@@ -0,0 +1,179 @@
+
+基本结构
+
+public class HelloWorld {
+    /* 第一个Java程序
+     * 它将输出字符串 Hello World
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World"); // 输出 Hello World
+    }
+}
+
+
+编译
+
+javac -encoding UTF-8 HelloWorld.java 
+java HelloWorld 
+
+
+
+对象
+
+public class Puppy{
+   public Puppy(String name){
+      //这个构造器仅有一个参数：name
+      System.out.println("小狗的名字是 : " + name ); 
+   }
+   public static void main(String[] args){
+      // 下面的语句将创建一个Puppy对象
+      Puppy myPuppy = new Puppy( "tommy" );
+   }
+}
+
+/* 实例化对象 */
+Object referenceVariable = new Constructor();
+/* 访问类中的变量 */
+referenceVariable.variableName;
+/* 访问类中的方法 */
+referenceVariable.methodName();
+
+
+
+
+打印
+
+System.out.println("变量值 : " + myPuppy.puppyAge ); 
+
+
+
+注释
+
+// 单行注释
+/*
+ Author by 菜鸟教程
+ 我是多行注释
+ */
+
+
+访问修饰符
+
+
+访问控制修饰符
+Java中，可以使用访问控制符来保护对类、变量、方法和构造方法的访问。Java 支持 4 种不同的访问权限。
+
+default (即默认，什么也不写）: 在同一包内可见，不使用任何修饰符。使用对象：类、接口、变量、方法。
+
+private : 在同一类内可见。使用对象：变量、方法。 注意：不能修饰类（外部类）
+
+public : 对所有类可见。使用对象：类、接口、变量、方法
+
+protected : 对同一包内的类和所有子类可见。使用对象：变量、方法。 注意：不能修饰类（外部类）。
+
+
+
+
+声明
+
+int a, b, c;         // 声明三个int型整数：a、 b、c
+int d = 3, e = 4, f = 5; // 声明三个整数并赋予初值
+byte z = 22;         // 声明并初始化 z
+String s = "runoob";  // 声明并初始化字符串 s
+double pi = 3.14159; // 声明了双精度浮点型变量 pi
+char x = 'x';        // 声明变量 x 的值是字符 'x'。
+
+
+
+
+
+条件
+
+if(布尔表达式){
+   //如果布尔表达式的值为true
+}else{
+   //如果布尔表达式的值为false
+}
+
+if(布尔表达式 1){
+   //如果布尔表达式 1的值为true执行代码
+}else if(布尔表达式 2){
+   //如果布尔表达式 2的值为true执行代码
+}else if(布尔表达式 3){
+   //如果布尔表达式 3的值为true执行代码
+}else {
+   //如果以上布尔表达式都不为true执行代码
+}
+
+switch(expression){
+    case value :
+       //语句
+       break; //可选
+    case value :
+       //语句
+       break; //可选
+    //你可以有任意数量的case语句
+    default : //可选
+       //语句
+}
+
+
+
+
+
+
+循环
+
+while( 布尔表达式 ) {
+  //循环内容
+}
+
+do {
+       //代码语句
+}while(布尔表达式);
+
+
+for(初始化; 布尔表达式; 更新) {
+    //代码语句
+}
+
+
+for(声明语句 : 表达式)
+{
+   //代码句子
+}
+int [] numbers = {10, 20, 30, 40, 50};
+for(int x : numbers ){
+ System.out.print( x );
+ System.out.print(",");
+}
+
+
+break;
+
+continue;
+
+
+
+
+
+
+
+
+
+函数
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
